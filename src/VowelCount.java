fb65f4f539f7f237626e8641c54406a1c1222ba1
Rauno <rauno.taaksalu@gmail.com>
2023-01-14 12:35:30 +0200

    added functions

--- src/VowelCount.java
+++ src/VowelCount.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class VowelCount {
 
     public void countVowels() {
+        System.out.println("Enter your word/sentence for the program to count the vowels:");
+//        Added the "sout" for user
 
         Scanner sc = new Scanner(System.in);
         int vowelCount = 0;
@@ -14,9 +16,11 @@ public class VowelCount {
         for (int i = 0; i < lowerCase.length(); i++) {
 
           if(lowerCase.charAt(i) == 'a' || lowerCase.charAt(i) == 'e' || lowerCase.charAt(i) == 'i' ||
-             lowerCase.charAt(i) == 'o' || lowerCase.charAt(i) == 'u') {
+             lowerCase.charAt(i) == 'o' || lowerCase.charAt(i) == 'u' || lowerCase.charAt(i) == 'õ' ||
+             lowerCase.charAt(i) == 'ü' || lowerCase.charAt(i) == 'ö' || lowerCase.charAt(i) == 'ä') {
              vowelCount++;
             }
+//          Added Ä, Ö, Ü and Õ to the list
         }
         System.out.println("There are " + vowelCount + " vowels in the given word/sentence.");
     }
